1bf65783b376ec6fa5ced7c544049d24ae564514
Fedchenko Artem <artem_fedchenko_2017@mail.ru>
2025-08-02 23:26:32 +0700

    feat: api запросы

---
+++ Client/src/shared/api/product.api.ts
@@ -0,0 +1,61 @@
+import type { Product } from "../types";
+import { ApiRequest } from "./requestTemplate";
+
+export const GetProducts = async () => {
+  try {
+    return await ApiRequest<Product[]>("/productTypes");
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const GetProductById = async (id: string) => {
+  try {
+    return await ApiRequest<Product>(`/productTypes/${id}`);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const CreateProduct = async (product: Omit<Product, "id">) => {
+  try {
+    return await ApiRequest<void>("/productTypes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const EditProduct = async (id: string, product: Partial<Omit<Product, "id">>) => {
+  try {
+    return await ApiRequest<void>(`/productTypes/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const DeleteProduct = async (id: string) => {
+  try {
+    return await ApiRequest<void>(`/productTypes/${id}`, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
